14958399536259b7f9ecb68f6bb513afaedbe7b8
Stanislas Marion <czzarr@users.noreply.github.com>
2018-09-07 14:20:08 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -39,6 +39,7 @@ brew install gnupg
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
 brew install grep
+brew install macvim
 brew install ncdu
 brew install openssh
 brew install screen
